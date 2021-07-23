cad0a35b2979f84b5629e824a7ba8da6578cc12f
Bhargava Vetsa <bhargavavetsa@Vetsas-MacBook-Pro.local>
2021-07-23 12:31:06 -0400

    removed comments

--- func_notes.py
+++ func_notes.py
@@ -1,17 +1,16 @@
-'''
+
 def myfunc(**kid):
   print(kid["fullname"])
 
 myfunc(fname = "Toby",lname = "Harris", fullname = "Toby Harris")
 #2 asterix means it can print any of the desired variables
-'''
-'''
+
 def myfunc1(kid):
   print(kid["fullname"])
 
 myfunc1({"fname":"Toby", "lname":"Harris", "fullname":"toby harris"})
-'''
-'''
+
+
 def my_function(country = "Norway"):
   print("I am from " + country)
 
@@ -20,4 +19,3 @@ my_function("India")
 my_function()
 my_function("Brazil")
 #by giving the variable a value when creating it if you dont use a value when calling it will use the already given value
-'''
