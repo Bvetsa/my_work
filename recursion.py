1a7b7c8b3f3048e682f53d9ddcb3aaf88072568e
Bhargava Vetsa <bhargavavetsa@Vetsas-MacBook-Pro.local>
2021-09-07 21:35:24 -0400

    website() added

--- recursion.py
+++ recursion.py
@@ -1,3 +1,4 @@
+
 def fact(n):
     if n == 1:
         return 1
@@ -14,3 +15,17 @@ def power(a,b):
         return power(a,b-1) * a
 
 print(power(2,10))
+
+def website(a):
+    import requests
+    import webbrowser
+    try:
+        r = requests.get(a)
+        print(r)
+        print("URL is valid and exists on the internet")
+        webbrowser.open(a)
+    except:
+        print("URL does not exist on Internet")
+        website(input("What URL would you like to look for. "))
+
+website(input("What URL would you like to look for. "))
